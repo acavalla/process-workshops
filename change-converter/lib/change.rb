1837b58cf34c0de772182640332bc86561587adf
A Cavalla <anna.cavalla@gmail.com>
2021-03-10 13:19:55 +0000

    refactored break_down method out of convert method

--- change-converter/lib/change.rb
+++ change-converter/lib/change.rb
@@ -1,34 +1,31 @@
 class Change
-  PENNY_DENOMS = [1, 2, 5, 10, 20, 50]
-  POUND_DENOMS = [1, 2, 5, 10, 20, 50]
+  attr_reader :result
+  DENOMS = [1, 2, 5, 10, 20, 50]
   def convert(amount)
+    result_array = []
     if !amount.is_a?Integer
       pounds = amount.to_s.split(".")[0].to_i
-      result = []
-      result_array = []
-      POUND_DENOMS.reverse_each do |coin|
-        if pounds >= coin
-          (pounds/coin).times { result << coin }
-          pounds -= (pounds/coin) * coin
-        end
+      break_down(pounds)
+      @result.each do |result|
+        result_array << "£" + result.to_s
       end
-        result.each do |result|
-          result_array << "£" + result.to_s
-        end
       return result_array
     else
-      result = []
-      result_array = []
-      PENNY_DENOMS.reverse_each do |coin|
-        if amount >= coin
-          (amount/coin).times { result << coin }
-          amount -= (amount/coin) * coin
-        end
-      end
-      result.each do |coin|
+      break_down(amount)
+      @result.each do |coin|
         result_array << coin.to_s + "p"
       end
       return result_array
+    end
+  end
+
+  def break_down(amount)
+    @result = []
+    DENOMS.reverse_each do |coin|
+      if amount >= coin
+        (amount/coin).times { result << coin }
+        amount -= (amount/coin) * coin
+      end
     end
   end
 end
